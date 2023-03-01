756f1408ba28c91aa2c0d9bf83951223abc187ee
Lukman Ernandi <48827979+Lukmanern@users.noreply.github.com>
2023-03-01 22:13:41 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+)
 
 func main() {
-	fmt.Println("hello world")
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(so socketio.Conn) error {
+		log.Println("Client connected")
+
+		// Send a message to the client when it connects
+		so.Emit("message", "Hello from server")
+
+		return nil
+	})
+
+	server.OnDisconnect("/", func(so socketio.Conn, reason string) {
+		log.Println("Client disconnected:", reason)
+	})
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	log.Println("Server listening on :3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
